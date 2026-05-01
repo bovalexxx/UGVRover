2f149bcf87973d1a543867ae9a19039a95c49ce2
bovalexxx <58110915+bovalexxx@users.noreply.github.com>
2026-05-01 11:39:19 +0300

    Cinemachine Camera installation
    - Cinemachine installation
    - Camera follow player

--- Assets/Scripts/VehicleController.cs
+++ Assets/Scripts/VehicleController.cs
@@ -34,7 +34,6 @@ namespace UGVRover
         public void Move(Vector2 direction)
         {
             Vector2 move = direction;
-            Debug.Log(move);
 
             float left = _settings.Speed * (move.y + move.x);
             float right = _settings.Speed * (move.y - move.x);
